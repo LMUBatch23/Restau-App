739ac3d31fa40a3f5afd62769b90be8f1b234531
ufimoni <ekongbert02@gmail.com>
2024-08-20 18:30:36 +0100

    The First commit

---
+++ test.java
@@ -0,0 +1,8 @@
+public class Restau {
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO OUR RESTAURANT_APPLICATION");
+	System.out.println("======================================");
+    }
+}
+// THIS IS A JAVA APPLICATION FOR TESTING.......
+// THIS WILL BE TESTED.
